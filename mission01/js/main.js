1cabca6d6cd0ff5d4df34a1f7d919ce6e54bfdeb
YANGJUNHO <yangwork96@gmail.com>
2024-02-02 23:41:11 +0900

    비밀번호 인풋 구축

--- mission01/js/main.js
+++ mission01/js/main.js
@@ -10,10 +10,14 @@ const user = {
 3. 상태 변수 관리
 4. 로그인 버튼을 클릭시 조건처리
 
+
 */
+
+// this.value.includes("!", "@", "#", "$", "%", "^", "*", "+", "=", "-")
 
 const email = document.querySelector("#userEmail");
 const userPassword = document.querySelector("#userPassword");
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "*", "+", "=", "-"];
 
 function emailReg(text) {
   if (this.value === user.id) {
@@ -23,7 +27,7 @@ function emailReg(text) {
     return re.test(String(text).toLowerCase());
   } else if (this.value.includes("@naver.com")) {
     email.classList.remove("is--invalid");
-  } else if (this.value[0]) {
+  } else if (this.value.length > 0) {
     email.classList.add("is--invalid");
   } else {
     email.classList.toggle("is--invalid");
@@ -36,17 +40,21 @@ function pwReg(text) {
     const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
     return re.test(String(text).toLowerCase());
   } else if (
-    this.value.includes("!", "@", "#", "$", "%", "^", "*", "+", "=", "-")
+    this.value.length >= 6 &&
+    specialCharacters.some((char) => this.value.includes(char))
+    // 이 부분은 mdn에서 .includes을 찾아보다가 Array 메서드를 쭉보고 .some을 활용했습니다
+    // 문법은 참고하여 사용하였습니다
+    // 문법 풀이는 더 공부하여 이해 해보겠습니다.
   ) {
     userPassword.classList.remove("is--invalid");
-  } else if (this.value[5] < this.value) {
-    userPassword.classList.remove("is--invalid");
-  } else if (this.value[0]) {
-    userPassword.classList.add("is--invalid");
-  } else {
+  } else if (this.value.length == 0) {
     userPassword.classList.toggle("is--invalid");
+  } else {
+    userPassword.classList.add("is--invalid");
   }
 }
 
 email.addEventListener("input", emailReg);
 userPassword.addEventListener("input", pwReg);
+
+["!", "@", "#", "$", "%", "^", "*", "+", "=", "-"];
